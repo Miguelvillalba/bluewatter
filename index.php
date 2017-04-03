5796f04ec2d027ab318da085bba66e2b341f5686
Miguelvillalba <1715110172@utec-tgo.edu.mx>
2017-04-03 13:35:30 -0500

    Update index.php

--- index.php
+++ index.php
@@ -8,15 +8,15 @@
     
 
        mysqli_select_db($connection, $db_name) or die("Error al seleccionar la base de datos:".mysqli_error());
-          @mysqli_query("SET NAMES 'utf8'");
+         @mysqli_query("SET NAMES 'utf8'");
 
       $idventa=$_POST['idventa'];
       $fecha=$_POST['fecha'];
       $nombre_restaurante=$_POST['nombre_restaurante'];
 
-          $sql_query = "insert into venta(idventa,fecha,nombre_restaurante) values ('".$idventa."','".$fecha."','".$nombre_restaurante."')";
-          $result = mysqli_query($connection, $sql_query);
-          $rows = array();
+         $sql_query = "insert into venta(idventa,fecha,nombre_restaurante) values ('".$idventa."','".$fecha."','".$nombre_restaurante."')";
+         $result = mysqli_query($connection, $sql_query);
+         $rows = array();
 while($r = mysqli_fetch_assoc($result)) {
   $rows[] = $r;
 }
