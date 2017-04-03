b1ad875c30096f824e2698dc535f3398ad37a8d0
Miguelvillalba <1715110172@utec-tgo.edu.mx>
2017-04-03 13:23:22 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,25 +1,25 @@
 <?PHP
-$hostname_localhost ="if0ck476y7axojpg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";  //nuestro servidor
-$database_localhost ="npzrrfv5h3vhmx0j";//Nombre de nuestra base de datos
-$username_localhost ="k91bsxha21dhruod";//Nombre de usuario de nuestra base de datos (yo utilizo el valor por defecto)
-$password_localhost ="krrfzclnhbt6jq9qc";//Contraseña de nuestra base de datos (yo utilizo por defecto)
-$localhost = mysql_connect($hostname_localhost,$username_localhost,$password_localhost)//Conexión a nuestro servidor mysql
-or
-trigger_error(mysql_error(),E_USER_ERROR); //mensaaje de error si no se puede conectar
-mysql_select_db($database_localhost, $localhost);//seleccion de la base de datos con la qu se desea trabajar
+  $db_host = "if0ck476y7axojpg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+        $db_name = "npzrrfv5h3vhmx0j";
+        $db_user = "k91bsxha21dhruod";
+        $db_password = "krrfzclnhbt6jq9qc3";
+    
+      $connection = mysqli_connect('if0ck476y7axojpg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'k91bsxha21dhruod', 'krrfzclnhbt6jq9qc3');
+    
 
-//variables que almacenan los valores que enviamos por nuestra app, (observar que se llaman igual en nuestra app y aqui)
+ mysqli_select_db($connection, $db_name) or die("Error al seleccionar la base de datos:".mysqli_error());
+    @mysqli_query("SET NAMES 'utf8'");
+
 $idventa=$_POST['idventa'];
 $fecha=$_POST['fecha'];
 $nombre_restaurante=$_POST['nombre_restaurante'];
 
-
-$query_search = "insert into venta(idventa,fecha,nombre_restaurante) values ('".$idventa."','".$fecha."','".$nombre_restaurante."')";//Sentencia sql a realizar
-$query_exec = mysql_query($query_search) or die(mysql_error());//Ejecuta la sentencia sql.
-$rows = array();
-
-
+    $sql_query = "insert into venta(idventa,fecha,nombre_restaurante) values ('".$idventa."','".$fecha."','".$nombre_restaurante."')";
+    $result = mysqli_query($connection, $sql_query);
+    $rows = array();
+while($r = mysqli_fetch_assoc($result)) {
+  $rows[] = $r;
+}
 print json_encode($rows);
 ?>
-
 
